de0d95cbcf777346f86a1528e8933c5508f83fd2
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-27 14:29:49 +0200

    worked with classes to add and remove a hiden clas

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal  = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const btnClosedModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function(){
+
+   
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+    }
+
+for(let i = 0; i < btnsOpenModal.length ; i++){
+    btnsOpenModal[i].addEventListener('click', openModal)
+
+}
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+btnClosedModal.addEventListener('click',closeModal)
+   
+
+
+overlay.addEventListener('click', closeModal)
